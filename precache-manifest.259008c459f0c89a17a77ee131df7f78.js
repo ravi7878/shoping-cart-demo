3aac9d5c8930b7e969a00aff70e715ccd0e00112
ravi7878 <ravi.kevadiya@iblinfotech.com>
2020-10-05 15:21:33 +0530

    Updates

---
+++ precache-manifest.259008c459f0c89a17a77ee131df7f78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87646644be7a41e561f6abc49f965ce8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "967eeafe34bb80ce8c4d",
+    "url": "/static/css/main.c24b858b.chunk.css"
+  },
+  {
+    "revision": "fa04b2d1bff18236f64d",
+    "url": "/static/js/2.8d371b82.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8d371b82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "967eeafe34bb80ce8c4d",
+    "url": "/static/js/main.a3e94f2e.chunk.js"
+  },
+  {
+    "revision": "ad3d3f1d34a73b724b01",
+    "url": "/static/js/runtime-main.b1bfe79c.js"
+  }
+]);
